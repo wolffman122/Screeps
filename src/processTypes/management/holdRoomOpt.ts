d1a25cea38e65e15292ee9a04a6ecb6d7ccaabf4
Jon Wolff <jon.wolff@wolfftech.com>
2020-01-19 20:57:11 -0500

    Hold Room Opt, nuke dismantling but not running yet

--- src/processTypes/management/holdRoomOpt.ts
+++ src/processTypes/management/holdRoomOpt.ts
@@ -15,6 +15,7 @@ export class HoldRoomOptManagementProcess extends Process
 {
   metaData: HoldRoomOptManagementProcessMetaData;
   type = 'hrmOpt'
+  spawnRoom: Room;
 
   ensureMetaData()
   {
@@ -22,6 +23,9 @@ export class HoldRoomOptManagementProcess extends Process
     {
       this.metaData.builderCreeps = [];
     }
+
+    if(!this.metaData.dismantlerCreeps)
+      this.metaData.dismantlerCreeps = [];
 
     if(!this.metaData.workerCreeps)
     {
@@ -76,6 +80,7 @@ export class HoldRoomOptManagementProcess extends Process
     }
 
     let spawnRoomName = this.metaData.flagName.split('-')[0];
+    this.spawnRoom = Game.rooms[spawnRoomName];
     let centerFlag = Game.flags['Center-'+spawnRoomName];
 
     if(this.metaData.roomName === undefined)
@@ -103,16 +108,19 @@ export class HoldRoomOptManagementProcess extends Process
     }
 
     let coreId: string;
-    if(Game.time % 10 === 8 && flag.room)
+    if(Game.time % 100 === 8 && flag.room)
     {
-      let structures = flag.room.find(FIND_HOSTILE_STRUCTURES, {filter: s => s.structureType === STRUCTURE_INVADER_CORE });
-      flag.memory.cores = structures.length ? true : false;
+      let cores = flag.room.find(FIND_HOSTILE_STRUCTURES, {filter: s => s.structureType === STRUCTURE_INVADER_CORE });
+      flag.memory.cores = cores.length ? true : false;
 
       if(flag.memory.cores)
       {
         //console.log("Hold room cores present" + flag.pos.roomName);
-        coreId = structures[0].id;
+        coreId = cores[0].id;
       }
+
+      let nuker = this.roomInfo(flag.room.name).nuker;
+      flag.memory.nuker = nuker ? true : false;
     }
 
     let defenderCount = 0;
@@ -210,6 +218,9 @@ export class HoldRoomOptManagementProcess extends Process
             }
           }
         }
+
+      //if(flag.memory.nuker && flag.room)
+        //this.DismantleNuker(flag);
 
 
     }
@@ -511,6 +522,90 @@ export class HoldRoomOptManagementProcess extends Process
         }
       }
 
+  }
+
+  DismantleNuker(flag: Flag)
+  {
+    try
+    {
+      const nuker = this.roomInfo(flag.room.name).nuker;
+      if((nuker?.store[RESOURCE_ENERGY] > 0 ?? 0) || (nuker?.store[RESOURCE_GHODIUM] ?? 0) === nuker.store.getCapacity(RESOURCE_GHODIUM))
+      {
+        this.metaData.dismantlerCreeps = Utils.clearDeadCreeps(this.metaData.dismantlerCreeps);
+        if(this.metaData.dismantlerCreeps.length < 1)
+        {
+          let creepName = 'hrm-dismantle-' + flag.pos.roomName + '-' + Game.time;
+          let spawned = Utils.spawn(
+            this.kernel,
+            this.spawnRoom.name,
+            'worker',
+            creepName,
+            {}
+          );
+
+          if(spawned)
+            this.metaData.dismantlerCreeps.push(creepName);
+        }
+      }
+      else
+      {
+        const ruins = flag.room.find(FIND_RUINS, {filter: r => r.structure.structureType === STRUCTURE_NUKER});
+        if(ruins.length)
+        {
+          const ruin = ruins[0];
+          const amount = (ruin.store[RESOURCE_ENERGY] ?? 0) + (ruin.store[RESOURCE_GHODIUM] ?? 0);
+          if(amount > 2000)
+          {
+
+          }
+        }
+      }
+
+
+
+      for(let i = 0; i < this.metaData.dismantlerCreeps.length; i++)
+      {
+        const creep = Game.creeps[this.metaData.dismantlerCreeps[i]];
+        if(creep)
+          this.DismantlerActions(creep, nuker);
+      }
+    }
+    catch(error)
+    {
+      console.log(this.name, 'DismantleNuker', error);
+    }
+  }
+
+  DismantlerActions(creep: Creep, nuker: StructureNuker)
+  {
+    try
+    {
+      if(nuker)
+      {
+        if(!creep.pos.isNearTo(nuker))
+          creep.travelTo(nuker);
+        else
+          creep.dismantle(nuker);
+
+        creep.say('💥');
+        return;
+      }
+      else
+      {
+        const spawn = this.roomData().spawns[0];
+        if(!creep.pos.isNearTo(spawn))
+          creep.travelTo(spawn);
+        else if(!spawn.spawning)
+          spawn.recycleCreep(creep);
+
+        creep.say('☠');
+        return;
+      }
+    }
+    catch (error)
+    {
+      console.log(this.name, 'DismantlerActions', error);
+    }
   }
 }
 
