89671b014b29f5e6a67114a3b5413ab9087be000
Jon Wolff <jon.wolff@wolfftech.com>
2020-04-14 14:08:06 -0400

    Power regen sources

--- src/processTypes/lifetimes/powerCreep.ts
+++ src/processTypes/lifetimes/powerCreep.ts
@@ -14,8 +14,6 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
 
   run()
   {
-    console.log(this.name, '??????????? Running ????????????')
-    console.log(this.name, global.test);
     const powerCreep = Game.powerCreeps[this.metaData.powerCreep];
     const room = Game.rooms[this.metaData.roomName];
     const factory = this.roomData().factory;
@@ -58,32 +56,6 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
       return;
     }
 
-    // console.log(this.name, 'Storage issue',
-    // storage.effects === undefined, storage.effects[PWR_OPERATE_STORAGE], storage.effects[PWR_OPERATE_STORAGE]?.ticksRemaining < 50)
-    // if(storage?.store.getUsedCapacity(RESOURCE_OPS) > 1000
-    //   && (storage.effects === undefined || storage.effects[PWR_OPERATE_STORAGE]?.ticksRemaining < 50))
-    //   {
-    //     if(powerCreep.store.getUsedCapacity(RESOURCE_OPS) < 100)
-    //     {
-    //       if(!powerCreep.pos.isNearTo(storage))
-    //         powerCreep.moveTo(storage);
-    //       else
-    //         powerCreep.withdraw(storage, RESOURCE_OPS, 100);
-
-    //       return;
-    //     }
-
-    //     if(!powerCreep.pos.inRangeTo(storage, 3))
-    //       powerCreep.moveTo(storage, {range: 3});
-    //     else
-    //       powerCreep.usePower(PWR_OPERATE_STORAGE, storage);
-
-    //     return;
-    //   }
-
-
-
-    console.log(this.name, factory.level, powerCreep.powers[PWR_OPERATE_FACTORY]);
     if(!factory.level && powerCreep.powers[PWR_OPERATE_FACTORY]
       && (powerCreep.store[RESOURCE_OPS] ?? 0) >= 300)
     {
@@ -101,7 +73,6 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
       if(factoryEffects?.length)
       {
         const effect = factoryEffects[0];
-        console.log(this.name, 'cooldown', powerCreep.powers[PWR_OPERATE_FACTORY].cooldown)
         if(effect.ticksRemaining < 20 && powerCreep.powers[PWR_OPERATE_FACTORY].cooldown === 0)
         {
           if(powerCreep.store.getUsedCapacity(RESOURCE_OPS) < 300)
@@ -124,6 +95,61 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
       }
     }
 
+    if(powerCreep.powers[PWR_OPERATE_EXTENSION]?.cooldown < 10
+      && powerCreep.store[RESOURCE_OPS] >= 2)
+    {
+      if(!room.memory.powerHarvesting)
+          room.memory.powerHarvesting = true;
+
+      let fillExt = false;
+      if(powerCreep.room.energyAvailable < (powerCreep.room.energyCapacityAvailable - 900))
+        fillExt = true;
+
+      if(fillExt)
+      {
+        if(!powerCreep.pos.inRangeTo(storage, 3))
+          powerCreep.moveTo(storage, {range: 3});
+        else
+          {
+            if(powerCreep.powers[PWR_OPERATE_EXTENSION]?.cooldown === 0)
+            {
+              powerCreep.usePower(PWR_OPERATE_EXTENSION, storage);
+              powerCreep.say('filRoom', true);
+
+            }
+          }
+        return;
+      }
+    }
+
+    if(powerCreep.powers[PWR_REGEN_SOURCE]?.cooldown < 10)
+    {
+      const sources = this.roomData().sources.filter(s =>
+        {
+          if(s.effects?.length === 0)
+            return true;
+          else
+          {
+            const effect = s.effects.filter(e => e.effect === PWR_REGEN_SOURCE && e.ticksRemaining < 15);
+            if(effect.length)
+              return true;
+          }
+        });
+
+      if(sources.length)
+      {
+        let target = powerCreep.pos.findClosestByPath(sources);
+
+        powerCreep.say('RS');
+        if(!powerCreep.pos.inRangeTo(target, 3))
+          powerCreep.moveTo(target, {range: 3});
+        else
+          powerCreep.usePower(PWR_REGEN_SOURCE, target);
+
+        return;
+      }
+    }
+
     // Go Generate ops in sk room
     if(powerCreep.store.getUsedCapacity() !== 0)
     {
@@ -134,37 +160,6 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
       }
     }
 
-    console.log(this.name, 'extensions', 1)
-    if(powerCreep.powers[PWR_OPERATE_EXTENSION]?.cooldown < 10
-      && powerCreep.store[RESOURCE_OPS] >= 2)
-      {
-        if(!room.memory.powerHarvesting)
-            room.memory.powerHarvesting = true;
-
-        console.log(this.name, 'extensions', 2)
-        let fillExt = false;
-        if(powerCreep.room.energyAvailable < (powerCreep.room.energyCapacityAvailable - 900))
-          fillExt = true;
-
-        console.log(this.name, 'extensions', 4)
-        if(fillExt)
-        {
-          console.log(this.name, 'extensions', 5)
-          if(!powerCreep.pos.inRangeTo(storage, 3))
-            powerCreep.moveTo(storage, {range: 3});
-          else
-            {
-              if(powerCreep.powers[PWR_OPERATE_EXTENSION]?.cooldown === 0)
-              {
-                powerCreep.usePower(PWR_OPERATE_EXTENSION, storage);
-                powerCreep.say('filRoom', true);
-
-              }
-            }
-          return;
-        }
-      }
-      console.log(this.name, 'extensions', 6)
     if(powerCreep.powers[PWR_GENERATE_OPS].cooldown === 0
       && powerCreep.store.getFreeCapacity() > 0)
     {
@@ -181,7 +176,5 @@ export class PowerCreepLifetimeProcess extends LifetimeProcess
       else
         powerCreep.transfer(storage, RESOURCE_OPS);
     }
-
   }
-
 }
